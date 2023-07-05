b443e6bf7a76f2ff3058c483707a5cf7310088f5
MoonHKLee <mf311110@gmail.com>
2023-07-05 18:54:28 +0900

    chore : 사용하지 않는 import 제거

--- src/test/java/lee/moonhyuk/blogsearch/acceptance/NaverBlogControllerTest.java
+++ src/test/java/lee/moonhyuk/blogsearch/acceptance/NaverBlogControllerTest.java
@@ -10,7 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class NaverBlogControllerTest extends ControllerTest {
     @Test
     void 블로그_검색_카카오_장애시_네이버() throws Exception {
-        블로그_검색("hello")
+        블로그_검색("naver")
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andReturn();
